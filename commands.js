cbfd67663f5ceeb86ef951ec0e1d7a742b973d74
Aaron Janse <gitduino@gmail.com>
2018-03-03 13:36:00 -0800

    add .xkcd fixes #1

--- commands.js
+++ commands.js
@@ -3,6 +3,8 @@ const Discord = require("discord.js");
 var AsciiTable = require('ascii-table')
 var crypto = require('crypto');
 
+var xkcd = require('xkcd');
+
 const commands = {
     help: {
         description: 'List all commands',
@@ -26,6 +28,25 @@ const commands = {
             })
         }
     },
+    xkcd: {
+        description: '',
+        execute: (msg, args) => {
+            function xkcd_handler(data) {
+                msg.channel.send(new Discord.RichEmbed()
+                    .setTitle(`#${data.num}: ${data.title}`)
+                    .setImage(data.img)
+                    .setFooter(data.alt)
+                    .setTimestamp(new Date(data.year, data.month - 1, data.day))
+                    .setURL(`https://xkcd.com/${data.num}`))
+            }
+
+            if (args) {
+                xkcd(parseInt(args[0]), xkcd_handler);
+            } else {
+                xkcd(xkcd_handler);
+            }
+        }
+    }
 }
 
 module.exports = commands
